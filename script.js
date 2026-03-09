72f1f43008c61062452a56fb2b5de13fc7d1bec1
Ujjal26 <156526573+Ujjal26@users.noreply.github.com>
2026-03-10 04:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const prices = document.querySelectorAll('.price');
+	let val=0;
+	prices.forEach((price) => {
+		val+=Number(price.textContent);
+	});
+	const table= document.querySelector('.table');
+	const tr = document.createElement('tr');
+	const td = document.createElement('td');
+
+	td.setAttribute('data-ns-test', 'grandTotal');
+	td.textContent = val;
+	
+    tr.appendChild(td);
+    table.appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
